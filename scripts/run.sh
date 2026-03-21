89c47dc4cc3187c041a3506e9cbf21e1f423e731
Liew Chen Tzer <liewct115@gmail.com>
2026-03-21 17:34:27 +0800

    change run.sh, to not automate the task and user can self input command

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,6 @@ echo "Running CLI simulation..."
 # Go to code folder
 cd code || { echo "Code folder not found"; exit 1; }
 
-# Feed commands to the CLI automatically
-# Example: 2 normal orders, 2 VIP orders, add 2 bots, show status, exit
-echo -e "normal\nvip\nnormal\nvip\naddbot\naddbot\nstatus\nexit" | ./app
-
-echo "CLI finished. Logs written to ../scripts/result.txt"
+#!/bin/bash
+echo "Starting CLI simulation..."
+./app
